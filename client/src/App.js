6e95f9f8b9dcea8b768ad21920eadd9ca96734ec
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2023-12-14 13:57:33 -0500

    Deleted files that are no longer needed, modified first page to have link to my linkedin profile

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Visit my linkedin profile and learn about me
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/ivanapejicic/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Ivana Pejicic
         </a>
       </header>
     </div>
