fde14bb0eb7fe2762f45eb1e34c05d4db7e977fa
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2023-12-15 11:00:26 -0500

    Title, subtitle and an imae added for the home page

--- client/src/assets/pages/Home/Home.js
+++ client/src/assets/pages/Home/Home.js
@@ -1,11 +1,19 @@
 import './Home.scss';
+import home_image from '../../icons/IMG_3082.jpeg';
 
 function Home() {
 
-    return(
-    <p>
-        hi
-    </p>
+    return (
+        <div className='home'>
+        <h1 className='home__title'>
+            Cafe Beignet
+        </h1>
+        <h2 className='home__subtitle'>
+            Miami
+        </h2>
+        <img className = 'home__image' src={home_image} alt = "street view from in front of cafe in South Beach"></img>
+        </div>
+
     )
 }
 
