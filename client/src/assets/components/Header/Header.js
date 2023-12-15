21262131305e106a31cdab2d1611e823713fd781
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2023-12-15 11:28:31 -0500

    Header component added. Created logo for the cafe. Colors that will be used are black and yellow

---
+++ client/src/assets/components/Header/Header.js
@@ -0,0 +1,12 @@
+import './Header.scss';
+import logo from '../../icons/logo.png';
+
+function Header() {
+    return (
+        <div className='header'>
+            <img className='header__logo' src={logo} alt="cafe beignet's logo, yellow coffee cup with cafe name under"></img> 
+        </div>
+    )
+}
+
+export default Header
--- client/src/assets/pages/Home/Home.js
+++ client/src/assets/pages/Home/Home.js
@@ -1,16 +1,13 @@
 import './Home.scss';
+import Header from '../../components/Header/Header';
 import home_image from '../../icons/IMG_3082.jpeg';
 
 function Home() {
 
     return (
+        
         <div className='home'>
-        <h1 className='home__title'>
-            Cafe Beignet
-        </h1>
-        <h2 className='home__subtitle'>
-            Miami
-        </h2>
+            <Header />
         <img className = 'home__image' src={home_image} alt = "street view from in front of cafe in South Beach"></img>
         </div>
 
