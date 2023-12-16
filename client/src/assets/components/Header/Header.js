6f8414e14bdfe4dcdd085b612b6d104b0e5591f3
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2023-12-16 16:11:12 -0500

    Added a nav bar to header. Each parts will take to a different page

--- client/src/assets/components/Header/Header.js
+++ client/src/assets/components/Header/Header.js
@@ -1,10 +1,19 @@
 import './Header.scss';
 import logo from '../../icons/logoBY.png';
+import {Link} from 'react-router-dom';
 
 function Header() {
     return (
         <div className='header'>
             <img className='header__logo' src={logo} alt="cafe beignet's logo, yellow coffee cup with cafe name under"></img> 
+            <div className='header__nav'>
+                <Link className='header__nav-a' to = '/about'>ABOUT</Link>
+                <Link className='header__nav-a' to = '/menu'>MENU</Link>
+                <Link className='header__nav-a' to ='https://ppolepizza.com/p-pole-points/'>POINTS</Link>
+                <Link className='header__nav-a' to='/contact'>CONTACT</Link>
+                <Link className='header__nav-a2' to='/order'>ORDER NOW</Link>
+
+            </div>
         </div>
     )
 }
