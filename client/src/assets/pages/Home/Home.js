5e1cc6d622e286643da792330ac6d7e0e58c6d04
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2023-12-17 11:14:14 -0500

    Styling for footer

--- client/src/assets/pages/Home/Home.js
+++ client/src/assets/pages/Home/Home.js
@@ -1,14 +1,16 @@
 import './Home.scss';
 import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
 import home_image from '../../icons/IMG_3082.jpeg';
 
 function Home() {
 
     return (
-        
+
         <div className='home'>
             <Header />
-        <img className = 'home__image' src={home_image} alt = "street view from in front of cafe in South Beach"></img>
+            <img className='home__image' src={home_image} alt="street view from in front of cafe in South Beach"></img>
+            <Footer />
         </div>
 
     )
