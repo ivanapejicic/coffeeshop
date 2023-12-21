62ff1007ff7310102477ee82e3504f91e6213517
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2023-12-20 22:57:16 -0500

    Contact page added

---
+++ client/src/assets/pages/Contact/Contact.js
@@ -0,0 +1,12 @@
+import './Contact.scss';
+import photo from '../../icons/beignetNutella.png'
+
+function Contact() {
+    return (
+        <div className='contact'>
+        </div>
+    )
+}
+
+
+export default Contact;
