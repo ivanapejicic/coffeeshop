2bec27c4ca8e411bd42e5476cf48090104ac855c
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2023-12-20 23:11:38 -0500

    Form for contacting the cafe added, needs a bit of styling:
    q
    q

--- client/src/assets/pages/Contact/Contact.js
+++ client/src/assets/pages/Contact/Contact.js
@@ -3,6 +3,9 @@ import logo from '../../icons/logoBY.png';
 import beignet from '../../icons/beignetNutella.png';
 
 function Contact() {
+    const handleSubmit = () => {
+        alert('Form submitted!');
+    };
     return (
         <div className='contact'>
             <div className='contact__top'>
@@ -11,6 +14,21 @@ function Contact() {
                 <img className='contact__top-logo' src={logo} alt="cafe beignet's logo"></img>
             </div>
             <img className='contact__img' src={beignet} alt="Beignet with powdered sugar on top"></img>
+            <form className='contact__form' onSubmit={handleSubmit}>
+                <label htmlFor='name'>Name *</label>
+                <input type='text' id='name' name='name' required />
+
+                <label htmlFor='email'>Email *</label>
+                <input type='email' id='email' name='email' required />
+
+                <label htmlFor='subject'>Subject</label>
+                <input type='text' id='subject' name='subject' />
+
+                <label htmlFor='message'>Message</label>
+                <textarea id='message' name='message'></textarea>
+
+                <button type='submit'>Submit</button>
+            </form>
         </div>
     )
 }
