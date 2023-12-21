db29233f9c8651105c9001249d271d061c830c8c
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2023-12-20 23:06:08 -0500

    Header and a beignet image added for contact page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './assets/pages/Home/Home';
 import Menu from './assets/pages/Menu/Menu';
+import Contact from './assets/pages/Contact/Contact';
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
         <Routes>
           <Route path= '' element={<Home />} />
           <Route path= '/menu' element={<Menu /> } />
+          <Route path= '/contact' element={<Contact /> } />
+
         </Routes>      
       </BrowserRouter>
     </div>
--- client/src/assets/pages/Contact/Contact.js
+++ client/src/assets/pages/Contact/Contact.js
@@ -1,9 +1,16 @@
 import './Contact.scss';
-import photo from '../../icons/beignetNutella.png'
+import logo from '../../icons/logoBY.png';
+import beignet from '../../icons/beignetNutella.png';
 
 function Contact() {
     return (
         <div className='contact'>
+            <div className='contact__top'>
+                <img className='contact__top-logo' src={logo} alt="cafe beignet's logo"></img>
+                <h1 className='contact__top-title'>CONTACT US</h1>
+                <img className='contact__top-logo' src={logo} alt="cafe beignet's logo"></img>
+            </div>
+            <img className='contact__img' src={beignet} alt="Beignet with powdered sugar on top"></img>
         </div>
     )
 }
