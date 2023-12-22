3ea4ca5646d696b8c9a66129fdc96511f95ce440
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2023-12-21 23:41:13 -0500

    Label styling

--- client/src/assets/pages/Contact/Contact.js
+++ client/src/assets/pages/Contact/Contact.js
@@ -14,18 +14,19 @@ function Contact() {
                 <img className='contact__top-logo' src={logo} alt="cafe beignet's logo"></img>
             </div>
             <img className='contact__img' src={beignet} alt="Beignet with powdered sugar on top"></img>
+            <h1 className='contact__title'>CONTACT</h1>
             <form className='contact__form' onSubmit={handleSubmit}>
                 <label htmlFor='name'>Name *</label>
-                <input type='text' id='name' name='name' required />
+                <input className='contact__form-field' type='text' id='name' name='name' required />
 
                 <label htmlFor='email'>Email *</label>
-                <input type='email' id='email' name='email' required />
+                <input className='contact__form-field' type='email' id='email' name='email' required />
 
                 <label htmlFor='subject'>Subject</label>
-                <input type='text' id='subject' name='subject' />
+                <input className='contact__form-field' type='text' id='subject' name='subject' />
 
                 <label htmlFor='message'>Message</label>
-                <textarea id='message' name='message'></textarea>
+                <textarea className='contact__form-area' id='message' name='message'></textarea>
 
                 <button type='submit'>Submit</button>
             </form>
