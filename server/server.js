f612792c9c915177bc8f83de0adcc50f8d6a1362
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2023-12-26 21:03:50 -0500

    Server running

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const PORT = process.env.PORT || 5050;
 
 app.use(cors());
 app.use(express.json());
+
+app.listen(PORT, console.log(`Server is running on port ${PORT}`));
+
