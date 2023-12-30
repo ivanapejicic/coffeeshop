7b9b8df928bb2d6b176f4c21be408d607854753a
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2023-12-30 15:00:38 -0500

    Added a few users (seeds file) to table points

---
+++ server/seeds/points.js
@@ -0,0 +1,26 @@
+exports.seed = async function (knex) {
+    // Deletes ALL existing entries
+    await knex('points').del();
+    return knex('points').insert([
+    {
+        phone: "3057481891",
+        points: 30,
+    },
+    {
+        phone: "95542132323",
+        points: 0,
+    },
+    {
+        phone: "3051231231",
+        points: 50,
+    },
+    {
+        phone: "3051231234",
+        points: 10,
+    },
+    {
+        phone: "7861231231",
+        points: 0,
+    },
+]);
+};
