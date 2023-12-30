28c33a622506375f1bdea84a4d31b18f4551ad15
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2023-12-30 14:51:26 -0500

    Table points has two columns only: user's phone number and points

--- server/migrations/20231230193848_points.js
+++ server/migrations/20231230193848_points.js
@@ -2,14 +2,18 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+    return knex.schema.createTable('points', (table) => {
+        table.string('phone').primary(); 
+        table.integer('points');
+    });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+    return knex.schema.dropTable('points');
+
 };
