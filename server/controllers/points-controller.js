310eaf1934fb77151086559aeadb1c9d4f698d9e
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2023-12-31 15:26:12 -0500

    Added a new endpoint using Knex to query the points table and find a user based on their phone number.

--- server/controllers/points-controller.js
+++ server/controllers/points-controller.js
@@ -1,2 +1,42 @@
 const knex = require('knex')(require('../knexfile'));
 
+const validatePhoneNumber = (data) => {
+    const requiredFields = [
+        'phone'
+    ];
+
+    for (const field of requiredFields) {
+        if (!data[field]) {
+            return {
+                valid: false,
+                message: `${field} is required.`,
+            };
+        }
+    }
+    return {
+        valid: true,
+    };
+};
+
+const findOne = async (req, res) => {
+    try {
+        const userFound = await knex("points").where({ phone: req.params.phone });
+
+        if (!userFound || userFound.length === 0) {
+            return res.status(404).json({
+                message: `User with phone number ${req.params.phone} not found`
+            });
+        }
+
+        const userData = userFound[0];
+        res.json(userData);
+    } catch (error) {
+        res.status(500).json({
+            message: `Unable to retrieve user data for user with phone number ${req.params.phone}`,
+        });
+    }
+};
+
+module.exports = {
+    findOne
+};
